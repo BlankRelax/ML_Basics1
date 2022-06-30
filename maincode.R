0c3c589c26f05b0ac859eee78c1982215551790f
Hassaan Mirza <84412764+BlankRelax@users.noreply.github.com>
2022-06-30 18:26:47 +0100

    F_1 predictor method

--- maincode.R
+++ maincode.R
@@ -3,6 +3,7 @@ library(dslabs)
 library(tidyverse)
 data(heights)
 
+
 y <- heights$sex #define predictors
 x <- heights$height #define outcomes
 
@@ -13,8 +14,8 @@ test_index <- createDataPartition(y, times = 1, p = 0.5, list = FALSE)
 train_set <- heights[-test_index, ]
 test_set <- heights[test_index, ]
 
-y_hat <- sample(c("Male", "Female"), length(test_index), replace = TRUE) %>% factor(levels = levels(test_set$sex)) #tests accuracy
-mean(y_hat == test_set$sex)
+#y_hat <- sample(c("Male", "Female"), length(test_index), replace = TRUE) %>% factor(levels = levels(test_set$sex)) #tests accuracy
+#mean(y_hat == test_set$sex)
 
 heights %>% group_by(sex) %>% summarise(mean(height), sd(height)) #this shows that men are on average taller than females
 
@@ -28,3 +29,18 @@ accuracy <- map_dbl(cutoff, function(x){
   mean(y_hat == train_set$sex)
 }) # map function applies every value in cutoff to the function
 accuracy[which.max(accuracy)]
+
+#now do this with our test set
+
+y_hat <- ifelse(x > cutoff[which.max(accuracy)], "Male", "Female") %>% factor(levels = levels(test_set$sex)) #Predict male if height is within two standard deviations from the average male.
+y_hat <- factor(y_hat)
+mean(y_hat == test_set$sex)
+
+
+cutoff <- seq(61, 70)
+F_1 <- map_dbl(cutoff, function(x){
+  y_hat <- ifelse(train_set$height > x, "Male", "Female") %>% factor(levels = levels(test_set$sex))
+  F_meas(data = y_hat, reference = factor(train_set$sex))
+}) # map function applies every value in cutoff to the function
+F_1[which.max(F_1)]
+cutoff[which.max(F_1)]
