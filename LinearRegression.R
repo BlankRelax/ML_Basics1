cc2571e0b8206f403f76b4170b1d9af4f94c21b5
Hassaan Mirza <84412764+BlankRelax@users.noreply.github.com>
2022-07-12 15:14:46 +0100

    rep_find_RMSE

--- LinearRegression.R
+++ LinearRegression.R
@@ -1,19 +1,42 @@
 library(tidyverse)
 library(caret)
 
-set.seed(1) # if using R 3.5 or earlier
-#set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
-n <- 100
-Sigma <- 9*matrix(c(1.0, 0.5, 0.5, 1.0), 2, 2)
-dat <- MASS::mvrnorm(n = 100, c(69, 69), Sigma) %>%
-  data.frame() %>% setNames(c("x", "y"))
-test_index <- createDataPartition(dat$y, times = 1, p=0.5, list = FALSE)
+#set.seed(1) # if using R 3.5 or earlier
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
 
-train_data <- dat[-test_index, ]
-test_data <- dat[test_index, ]
+find_RMSE <- function(ds){
+  test_index <- createDataPartition(ds$y, times = 1, p=0.5, list = FALSE)
+  train_data <- dat %>% slice(-test_index)
+  test_data <- dat %>% slice(test_index)
+  fit <- lm(y ~ x, data = train_data)
+  y_hat <- predict(fit, newdata = test_data)
+  sqrt(mean((y_hat-test_data$y)^2))
+}
+ 
+
+find_RMSE_of_n <- function(n){
+  Sigma <- 9*matrix(c(1.0, 0.5, 0.5, 1.0), 2, 2)
+  dat <- MASS::mvrnorm(n , c(69, 69), Sigma) %>%
+    data.frame() %>% setNames(c("x", "y"))
+    find_RMSE(dat)
+  
+}
 
 
-y_hat <- predict(lm(train_data), test_data)
-mean((y_hat - test_data$y)^2)
 
-RMSE(test_data$y, y_hat)
+rep_find_RMSE <- function(x){
+  data.frame(mean_RMSE =  replicate(100, find_RMSE_of_n(x))) %>% summarise(., mean(.$mean_RMSE), sd(.$mean_RMSE))
+}
+
+
+
+
+n <- c(100, 500, 1000, 5000, 10000)
+print(n)
+set.seed(1, sample.kind="Rounding")
+rep_find_RMSE(100)
+
+
+#rlang::last_error()
+
+
